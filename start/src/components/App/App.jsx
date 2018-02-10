6f592798f3a254381a9ce5a7fed68cdd87364159
MikhailZharkov <zharkov@abcreg.ru>
2018-02-10 20:26:58 +0300

    uncomment

--- start/src/components/App/App.jsx
+++ start/src/components/App/App.jsx
@@ -19,13 +19,13 @@ export default class App extends Component {
     render() {
         return (
             <div className='main'>
-                {/* <header>
+                <header>
                     <h1>
                         Hello World
                     </h1>
                     <MySVG svg={logo} size={'max'} />
                 </header>
-                <Clock /> */}
+                <Clock />
                 <Secret />
                 <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
             </div>
